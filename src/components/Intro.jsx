f7d91088c362394ae8c95f96cfad56bee8593f0f
jpromanonet <programming@jpromano.net>
2022-12-18 19:09:46 -0300

    intro done

---
+++ src/components/Intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Intro() {
+   return (
+      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">La lista autodidacta</h1>
+         <p className="text-base md:text-xl mb-3 font-medium">Aprende a programar gratis y de forma autodidacta</p>
+         <p className="text-sm max-w-xl mb-6 font-bold">
+            La lista autodidacta, es eso una lista super simple de recursos gratis (sitios web, libros, tutoriales, etc) para aprender a programar gratis y de forma audidacta.
+            <br />
+            <br />
+            Algunos de los recursos extienden certificaciones para agregar a tu CV y otros no, pero lo fundamental es que indistintamente de los recursos podamos llevarnos ejercicios y proyectos terminados que nos ayuden a practicar.
+         </p>
+      </div>
+   )
+}
+
+export default Intro;
