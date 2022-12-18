6c5da713bae7216ca87c56173d656c8e997217ba
jpromanonet <programming@jpromano.net>
2022-12-18 19:14:46 -0300

    Working on timeline

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ function Footer() {
 								</svg>
 							</a>
 							<a 
-								href="https://linkedin.com/in/forrestpknight"
+								href="https://linkedin.com/in/jpromanonet"
 								className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 								<svg
 								className="w-5 h-5 fill-current"
