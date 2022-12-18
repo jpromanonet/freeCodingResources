24b5fdb82b0198cbfb207b5f53dcf74374996984
jpromanonet <programming@jpromano.net>
2022-12-18 19:11:48 -0300

    Working on timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -7,7 +7,7 @@ function Timeline() {
    return (
       <div className="flex flex-col md:flex-row justify-center my-20">
          <div className="w-full md:w-7/12">
-            <Title>Timeline</Title>
+            <Title>Mapa de ruta sugerido</Title>
             {timeline.map(item => (
                <TimelineItem 
                   year={item.year}
