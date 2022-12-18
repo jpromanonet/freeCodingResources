97955f1fd472d981cc574288fbbd41615a9ba001
jpromanonet <programming@jpromano.net>
2022-12-18 19:45:03 -0300

    Working on timeline

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
    {
       title: 'Recurso 01',
-      imgUrl: '/assets/terminal-website.jpg',
+      imgUrl: '/assets/freecodecamp.png',
       stack: ['HTML', 'CSS', 'JavaScript'],
       link: 'https://freecodecamp',
    },
    {
       title: 'Recurso 02',
       imgUrl: '/assets/dev-portfolio.jpg',
-      stack: ['React', 'Tailwind', 'Vite'],
-      link: 'https://thehackershideout.com',
+      stack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Tutorials'],
+      link: 'https://theodinproject.org',
    },
    {
       title: 'AI Nerf Gun',
-      imgUrl: '/assets/ai-nerf-gun.jpg',
-      stack: ['Python', 'C++', 'Robotics'],
-      link: 'https://youtu.be/A8eegxL82PI',
+      imgUrl: '/assets/theodinproject.png',
+      stack: ['Ruby On Rails', 'JavaScript', 'HTML', 'CSS', 'Git', 'Shell'],
+      link: 'https://www.theodinproject.com',
    },
 ]
