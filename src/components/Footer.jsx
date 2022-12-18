ca3b07c832f28a24dfdb7e1d440ce9bda22eb0aa
jpromanonet <programming@jpromano.net>
2022-12-18 19:13:58 -0300

    Working on timeline

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,8 @@ function Footer() {
 					<div>
 						<div className="flex flex-wrap justify-center gap-2">
 							<a 
-								href="https://twitter.com/forrestpknight"
+								href="https://twitter.com/jpromanonet"
+								target="_blank"
 								className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
 								<svg
 									className="w-5 h-5 fill-current"
