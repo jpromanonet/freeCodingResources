6b733641b22953febabb0411b688f6599ee1b53b
jpromanonet <programming@jpromano.net>
2022-12-24 12:58:05 -0300

    Almost done

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Intro() {
    return (
-      <div className="flex items-center justify-left flex-col text-left pt-20 pb-6">
-         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">La lista autodidacta</h1>
+      <div className="flex items-left justify-left flex-col text-left pt-20 pb-12">
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-6 font-bold">La lista autodidacta</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Aprende a programar gratis y de forma autodidacta</p>
-         <p className="text-sm max-w-xl mb-6 font-bold">
+         <p className="text-m max-w-xl mb-6 font-bold">
             La lista autodidacta, es eso una lista super simple de recursos gratis (sitios web, libros, tutoriales, etc) para aprender a programar gratis y de forma audidacta.
             <br />
             <br />
@@ -14,6 +14,7 @@ function Intro() {
             <br />
             Te recomiendo seguir el "Mapa de ruta sugerido" pero si no te sentis comode, arranca por donde más te llame la atención o te genere más curiosidad.
          </p>
+         <hr/>
       </div>
    )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -16,6 +16,7 @@ function Portfolio() {
             ))}
          </div>
       </div>
+      <hr>
    )
 }
 
