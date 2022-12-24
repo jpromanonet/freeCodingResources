6f467667d3aaace698e9f755a74aa57e5715c45a
jpromanonet <programming@jpromano.net>
2022-12-23 21:29:49 -0300

    Almost done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -61,7 +61,7 @@ function Footer() {
 					</div>
 				</div>
 			<p className="text-sm mt-2  opacity-50">
-				&copy; {new Date().getFullYear()}  Juan P. Romano. Non rights reserved, steal it and be happy.
+				&copy; {new Date().getFullYear()}  Juan P. Romano. ningun derecho reservador, robalo y se feliz.
 			</p>
 		</div>
 	);
