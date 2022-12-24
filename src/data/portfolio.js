968cb842848bc96b09a07d898ab1ea29826b2cca
jpromanonet <programming@jpromano.net>
2022-12-24 19:35:06 -0300

    Almost done

---
+++ src/data/portfolio.js
@@ -0,0 +1,44 @@
+export default [
+   {
+      title: 'Free Code Camp',
+      cert: 'Con certificación',
+      imgUrl: '/assets/freecodecamp.png',
+      stack: ['HTML', 'CSS', 'JavaScript', 'React JS', 'Node JS', 'Express', 'MongoDB', 'SQL', 'Glitch', 'Git', 'Python', 'AWS'],
+      link: 'https://freecodecamp',
+   },
+   {
+      title: 'The Odin Project',
+      cert: 'Sin certificación',
+      imgUrl: '/assets/theodinproject.png',
+      stack: ['Ruby On Rails', 'JavaScript', 'HTML', 'CSS', 'Git', 'Shell'],
+      link: 'https://www.theodinproject.com',
+   },
+   {
+      title: 'Sololearn',
+      cert: 'Con certificación',
+      imgUrl: '/assets/sololearn.png',
+      stack: ['JavaScript', 'HTML', 'CSS', 'C#', 'PHP', 'C++', 'C', 'R', 'Go', 'SQL','Python', 'Java', 'React', 'Redux', 'Machine Learning','Mobile Apps', 'Video Games'],
+      link: 'https://www.sololearn.com',
+   },
+   {
+      title: 'Code Gym',
+      cert: 'Sin certificación',
+      imgUrl: '/assets/codegym.png',
+      stack: ['Java', 'JVM', 'JWS', 'JSP', 'T-SQL'],
+      link: 'https://www.codegym.cc',
+   },
+   {
+      title: 'Free Code Camp (Youtube)',
+      cert: 'Sin certificación',
+      imgUrl: '/assets/freecodecampyt.png',
+      stack: ['HTML', 'CSS', 'JavaScript', 'React JS', 'Node JS', 'Express', 'MongoDB', 'SQL', 'Glitch', 'Git', 'Python', 'AWS', 'Everything about tech'],
+      link: 'https://www.codegym.cc',
+   },
+   {
+      title: 'Teach yourself CS',
+      cert: 'Sin certificación',
+      imgUrl: '/assets/teachyourselfcs.png',
+      stack: ['Programming', 'CS Architecture', 'Algorithms', 'Data Structures', 'Math for CS', 'Operating Systems', 'Databases', 'Networking'],
+      link: 'https://www.teachyourselfcs.com',
+   },
+]
