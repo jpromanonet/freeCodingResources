c327f09972af429f5fe4c6aa3df829f802c09a2c
jpromanonet <programming@jpromano.net>
2022-12-24 13:59:02 -0300

    Almost done

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,17 +2,11 @@ export default [
    {
       title: 'Free Code Camp',
       imgUrl: '/assets/freecodecamp.png',
-      stack: ['HTML', 'CSS', 'JavaScript'],
+      stack: ['HTML', 'CSS', 'JavaScript', 'React JS'],
       link: 'https://freecodecamp',
    },
    {
-      title: 'Recurso 02',
-      imgUrl: '/assets/ania.png',
-      stack: ['React', 'JavaScript', 'HTML', 'CSS', 'Node.js', 'Tutorials'],
-      link: 'https://https://www.youtube.com/@AniaKubow',
-   },
-   {
-      title: 'AI Nerf Gun',
+      title: 'The Odin Project',
       imgUrl: '/assets/theodinproject.png',
       stack: ['Ruby On Rails', 'JavaScript', 'HTML', 'CSS', 'Git', 'Shell'],
       link: 'https://www.theodinproject.com',
