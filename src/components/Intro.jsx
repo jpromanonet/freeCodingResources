37a5d4ab4ae6fb9cfcd90125e45385ebb7df40c7
jpromanonet <programming@jpromano.net>
2022-12-24 12:53:21 -0300

    Almost done

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Intro() {
    return (
-      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <div className="flex items-center justify-left flex-col text-left pt-20 pb-6">
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">La lista autodidacta</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Aprende a programar gratis y de forma autodidacta</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -5,8 +5,8 @@ import Title from './Title';
 
 function Timeline() {
    return (
-      <div className="flex flex-col md:flex-row justify-center my-20">
-         <div className="w-full md:w-7/12">
+      <div className="flex flex-col md:flex-row justify-left my-20">
+         <div className="w-full md:w-12/12">
             <Title>Mapa de ruta sugerido</Title>
             {timeline.map(item => (
                <TimelineItem 
