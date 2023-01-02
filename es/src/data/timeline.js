7a7a3f2885dd051c367515f6dc27fd83a63b3236
jpromanonet <programming@jpromano.net>
2023-01-02 16:53:09 -0300

    Timeline

--- es/src/data/timeline.js
+++ es/src/data/timeline.js
@@ -1,14 +1,14 @@
 export default [
    {
       step: '1°',
-      title: 'Fundamentos de programación',
+      title: 'Inmersión',
       details: 
-         'En esta etapa inicial es recomendable hacer el curso de Harvard CS50 de introducción a ciencias de la computación y el primer curso de Free Code Camp llamado "Responsive Web Design"',
+         'En esta etapa inicial es recomendable hacer el curso de Harvard CS50 de introducción a ciencias de la computación y el primer curso de Free Code Camp llamado "Responsive Web Design" ya que en esta etapa vamos a ver si nos gusta programar o elegimos ir hacia otra area de especialidad en tecnologia',
    },
    {
       step: '2°',
       title: '',
       details: 
-         'Algo va a aca',
+         'Algo va a ',
    },
 ]
