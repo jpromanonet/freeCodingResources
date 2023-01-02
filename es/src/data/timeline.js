2e063b6a45f3e92ac38ce7972637df3928e2527e
jpromanonet <programming@jpromano.net>
2023-01-02 16:51:24 -0300

    Timeline

--- es/src/data/timeline.js
+++ es/src/data/timeline.js
@@ -3,6 +3,12 @@ export default [
       step: '1°',
       title: 'Fundamentos de programación',
       details: 
+         'En esta etapa inicial es recomendable hacer el curso de Harvard CS50 de introducción a ciencias de la computación y el primer curso de Free Code Camp llamado "Responsive Web Design"',
+   },
+   {
+      step: '2°',
+      title: '',
+      details: 
          'Algo va a aca',
    },
 ]
