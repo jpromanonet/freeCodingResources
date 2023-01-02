d835dd24804428cbad04d440ed35061140743915
jpromanonet <programming@jpromano.net>
2023-01-02 17:05:13 -0300

    Timeline

--- es/src/data/timeline.js
+++ es/src/data/timeline.js
@@ -1,14 +1,26 @@
 export default [
    {
       step: '1°',
-      title: 'Inmersión',
+      title: 'Fundamentos',
       details: 
-         'En esta etapa inicial es recomendable hacer el curso de Harvard CS50 de introducción a ciencias de la computación y el primer curso de Free Code Camp llamado "Responsive Web Design" ya que en esta etapa vamos a ver si nos gusta programar o elegimos ir hacia otra area de especialidad en tecnologia',
+         'En esta etapa inicial es recomendable hacer el curso de Harvard CS50 de introducción a ciencias de la computación, el primer curso de Free Code Camp llamado "Responsive Web Design" y el libro interactivo de Al Sweigart "Automating the boring stuff with python" ya que en esta etapa vamos a ver si nos gusta programar o elegimos ir hacia otra area de especialidad en tecnologia',
    },
    {
       step: '2°',
-      title: '',
+      title: 'Aprender haciendo',
       details: 
-         'Algo va a ',
+         '¿Hiciste la primera parte y te gusto?, ¡Genial!, es hora de hacer cosas =) y para eso los recursos recomendados son Free Code Camp (hacer el resto de certificaciones y seguir tutoriales del canal de Youtube, el sitio de Sololearn para certificarte en la sintaxis de algunos lenguajes, comenzar The Odin Project o revisar Code Gym para aprender Java ',
+   },
+   {
+      step: '3°',
+      title: 'La vieja confiable (CRUDs)',
+      details: 
+         'Si hay algo que nos van a pedir que hagamos en cualquier trabajo de desarrollador son ABMs (Altas Bajas Modificaciones) o en inglés CRUDL (Create, Read, Update, Delete, List ) y sin duda alguna, Oscar de Develoteca, el master of cruds nos va a enseñar a hacerlos en todos los lenguajes que quieras. ',
+   },
+   {
+      step: '4°',
+      title: 'Haciendo proyectos lindos n.n',
+      details: 
+         'Ok, ya estas aca y es hora de hacer tus propios proyectos y para eso tenes varios canales, el que más hands-ons en materia de video juegos y clones de plataformas conocidas es el de la muy genial Ania Kubow (imaginate tener un clon de Netflix o Disney+ em tu portfolio), también podes revisar el curso de Hardvard CS50 orientado al desarrollo de video juegos o "The Big Book of Small Python Projects" asi como "Invent your own computer games" ambos de Al Sweigart',
    },
 ]
