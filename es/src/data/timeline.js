285ddd755f164eeba66d2c4c88b8c64b3d3862e7
jpromanonet <programming@jpromano.net>
2023-01-02 17:17:49 -0300

    Timeline

--- es/src/data/timeline.js
+++ es/src/data/timeline.js
@@ -9,7 +9,7 @@ export default [
       step: '2°',
       title: 'Aprender haciendo',
       details: 
-         '¿Hiciste la primera parte y te gusto?, ¡Genial!, es hora de hacer cosas =) y para eso los recursos recomendados son Free Code Camp (hacer el resto de certificaciones y seguir tutoriales del canal de Youtube, el sitio de Sololearn para certificarte en la sintaxis de algunos lenguajes, comenzar The Odin Project o revisar Code Gym para aprender Java ',
+         '¿Hiciste la primera parte y te gusto?, ¡Genial!, es hora de hacer cosas =) y para eso los recursos recomendados son Free Code Camp (hacer el resto de certificaciones y seguir tutoriales del canal de Youtube, el canal de Beau Carnes, el sitio de Sololearn para certificarte en la sintaxis de algunos lenguajes, comenzar The Odin Project o revisar Code Gym para aprender Java ',
    },
    {
       step: '3°',
@@ -21,6 +21,24 @@ export default [
       step: '4°',
       title: 'Haciendo proyectos lindos n.n',
       details: 
-         'Ok, ya estas aca y es hora de hacer tus propios proyectos y para eso tenes varios canales, el que más hands-ons en materia de video juegos y clones de plataformas conocidas es el de la muy genial Ania Kubow (imaginate tener un clon de Netflix o Disney+ em tu portfolio), también podes revisar el curso de Hardvard CS50 orientado al desarrollo de video juegos o "The Big Book of Small Python Projects" asi como "Invent your own computer games" ambos de Al Sweigart',
+         'Ok, ya estas aca y es hora de hacer tus propios proyectos y para eso tenes varios canales, el que más hands-ons en materia de video juegos y clones de plataformas conocidas es el de la muy genial Ania Kubow (imaginate tener un clon de Netflix o Disney+ em tu portfolio) y si tenes ganas/dinero podes comprar su curso en el sitio web oficial pero no es necesario, también podes revisar el curso de Hardvard CS50 orientado al desarrollo de video juegos, "The Big Book of Small Python Projects" asi como "Invent your own computer games" ambos de Al Sweigart e inclusive si Python nos viene gustando mucho podemos hacer los proyectos del canal "Just Django" o comprar su path en el sitio web aunque nuevamente no es necesario.',
+   },
+   {
+      step: '5°',
+      title: 'Momento de ascender',
+      details: 
+         '¡Felicitaciones! ya sabes fundamentos de programación y claramente si llegaste hasta aca te encanta programar asi que es momento de tomar una ruta mas solemne y solitaria donde buscamos no solo aumentar tus skills sino hacer proyectos que nos rompan la cabeza y para eso tenemos varios recursos tales como "Coding Interview University", "Beyond The Basic Stuff with Python" o los geniales juegos y motores de juegos de "One Lone Coder" en C++ como para variar un poco',
+   },
+   {
+      step: '6°',
+      title: 'Tiempo de leer mucho (¡pero mucho eh!)',
+      details: 
+         'Ya hiciste muchos proyectos hermosos con Ania, Al, Javi de One Lone Coder u Oscar de Develoteca PERO como todo, es necesario no solo tener bases teoricas solidas sino que reforzarlas con más contenido y sobretodo de buena calidad por lo cual en esta instancia y previo a desarrollar software mucho más complejo de lo que imaginabas en la primer etapa es necesario que te pongas a estudiar los libros mencionados en teachyourselfcs.com ya que vas a tener un conocimiento general y profundo de Redes, Bases de Datos, Sistemas Operativos, compiladores, lenguajes de programación y algoritmos',
+   },
+   {
+      step: '7°',
+      title: 'The Final Boss',
+      details: 
+         'Para la etapa final solo voy a escribir "Write your own operative system" y "Build your own X" y con eso me despido <3',
    },
 ]
